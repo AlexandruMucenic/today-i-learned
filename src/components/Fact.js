614cf0c054b0518e01c28a8ec4898f4aaf25d708
“AlexandruMucenic” <alexmucenic6@gmail.com>
2022-12-31 17:45:43 +0200

    created fact component

---
+++ src/components/Fact.js
@@ -0,0 +1,44 @@
+function Fact({ fact }) {
+  const CATEGORIES = [
+    { name: 'technology', color: '#3b82f6' },
+    { name: 'science', color: '#16a34a' },
+    { name: 'finance', color: '#ef4444' },
+    { name: 'society', color: '#eab308' },
+    { name: 'entertainment', color: '#db2777' },
+    { name: 'health', color: '#14b8a6' },
+    { name: 'history', color: '#f97316' },
+    { name: 'news', color: '#8b5cf6' },
+  ];
+
+  return (
+    <li className='fact'>
+      <p>
+        {fact.text}
+        <a
+          className='source'
+          href={fact.source}
+          target='_blank'
+          rel='noreferrer'
+        >
+          (Source)
+        </a>
+      </p>
+      <span
+        className='tag'
+        style={{
+          backgroundColor: CATEGORIES.find(cat => cat.name === fact.category)
+            .color,
+        }}
+      >
+        {fact.category}
+      </span>
+      <div className='vote-buttons'>
+        <button>👍 {fact.votesInteresting}</button>
+        <button>🤯 {fact.votesMindblowing}</button>
+        <button>⛔ {fact.votesFalse}</button>
+      </div>
+    </li>
+  );
+}
+
+export default Fact;
--- src/components/FactsList.js
+++ src/components/FactsList.js
@@ -1,3 +1,5 @@
+import Fact from './Fact';
+
 const initialFacts = [
   {
     id: 1,
@@ -32,17 +34,6 @@ const initialFacts = [
   },
 ];
 
-const CATEGORIES = [
-  { name: 'technology', color: '#3b82f6' },
-  { name: 'science', color: '#16a34a' },
-  { name: 'finance', color: '#ef4444' },
-  { name: 'society', color: '#eab308' },
-  { name: 'entertainment', color: '#db2777' },
-  { name: 'health', color: '#14b8a6' },
-  { name: 'history', color: '#f97316' },
-  { name: 'news', color: '#8b5cf6' },
-];
-
 function FactList() {
   const facts = initialFacts;
 
@@ -50,35 +41,9 @@ function FactList() {
     <section>
       <ul className='facts-list'>
         {facts.map(fact => (
-          <li key={fact.id} className='fact'>
-            <p>
-              {fact.text}
-              <a
-                className='source'
-                href={fact.source}
-                target='_blank'
-                rel='noreferrer'
-              >
-                (Source)
-              </a>
-            </p>
-            <span
-              className='tag'
-              style={{
-                backgroundColor: CATEGORIES.find(
-                  cat => cat.name === fact.category
-                ).color,
-              }}
-            >
-              span
-            </span>
-            <div className='vote-buttons'>
-              <button>👍 {fact.votesInteresting}</button>
-              <button>🤯 {fact.votesMindblowing}</button>
-              <button>⛔ {fact.votesFalse}</button>
-            </div>
-          </li>
+          <Fact key={fact.id} fact={fact} />
         ))}
+        <p>There are {facts.length} facts in the database. Add your own!</p>
       </ul>
     </section>
   );
