8f836a0a801285c99bf70f9c72307a1bcdee864b
“AlexandruMucenic” <alexmucenic6@gmail.com>
2023-01-02 17:35:17 +0200

    update NewFactForm  component

--- src/components/NewFactForm.js
+++ src/components/NewFactForm.js
@@ -1,15 +1,42 @@
+import { useState } from 'react';
+import { CATEGORIES } from '../Categories';
+
 function NewFactForm() {
+  const [text, setText] = useState('');
+  const [source, setSource] = useState('');
+  const [category, setCategory] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <form className='fact-form'>
-      <input type='text' placeholder='Share a fact with the world...' />
-      <span>200</span>
-      <input type='text' placeholder='Trustworthy source...' />
-      <select name='' id=''>
+    <form className='fact-form' onSubmit={handleSubmit}>
+      <input
+        type='text'
+        placeholder='Share a fact with the world...'
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+
+      <span>{200 - text.length}</span>
+
+      <input
+        type='text'
+        placeholder='Trustworthy source...'
+        value={source}
+        onChange={e => setSource(e.target.value)}
+      />
+
+      <select value={category} onChange={e => setCategory(e.target.value)}>
         <option value=''>Choose category:</option>
-        <option value='technology'>Technology</option>
-        <option value='science'>Science</option>
-        <option value='finance'>Finance</option>
+        {CATEGORIES.map(cat => (
+          <option key={cat.name} value={cat.name}>
+            {cat.name.toUpperCase()}
+          </option>
+        ))}
       </select>
+
       <button className='btn btn-large'>Post</button>
     </form>
   );
