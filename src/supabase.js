67658984cbae11c156a07bdbe9ab067c942a7178
“AlexandruMucenic” <alexmucenic6@gmail.com>
2023-01-27 14:05:56 +0200

    updated supabase key name

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://ywxvwggxhefhwereqzam.supabase.co';
-const supabaseKey = process.env.SUPABASE_KEY;
+const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
