772a33087978de1d3c86e2295ea1f60000df4dff
“AlexandruMucenic” <alexmucenic6@gmail.com>
2023-01-27 14:02:13 +0200

    updated supabase key

--- src/supabase.js
+++ src/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://eneqnchgbdkxolkvvtxe.supabase.co';
-const supabaseKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVuZXFuY2hnYmRreG9sa3Z2dHhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzE3ODI3OTIsImV4cCI6MTk4NzM1ODc5Mn0.rbLwYC34fi3LBB9mqUpLrAnsaAp-6u8IhJySQ_kfDgE';
+const supabaseUrl = 'https://ywxvwggxhefhwereqzam.supabase.co';
+const supabaseKey = process.env.SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
